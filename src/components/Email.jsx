1fba3a3b71c9aa6e920e5317d2f03baeb63ee590
shri1906 <rajm46069@gmail.com>
2025-01-03 14:49:44 +0530

    mail issue resolved

--- src/components/Email.jsx
+++ src/components/Email.jsx
@@ -1,45 +1,88 @@
-import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
-import { Box, Input, Text } from '@chakra-ui/react';
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import { Box,  Text, Spinner } from "@chakra-ui/react";
 import styles from "../styles/Email.module.css";
 
-
 export const Email = () => {
-    const form = useRef();
+  const form = useRef();
+  const [status, setStatus] = useState("");
+  const [loading, setLoading] = useState(false);
 
-    const sendEmail = (e) => {
-      e.preventDefault();
-  
-      emailjs.sendForm(
-        'service_81lqksr',
-        'template_ggv390v',
-        form.current, 
-        'p4F-g1ScE3LCTgfLp')
-        .then((result) => {
-            console.log(result.text);
-            alert("mail sent!");
-        }, (error) => {
-            console.log(error.text);
-        });
-    };
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setLoading(true); 
+
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        form.current,
+        process.env.REACT_APP_PUBLIC_KEY
+      )
+      .then(
+        () => {
+          setStatus("Email Sent Successfully!");
+          setLoading(false); 
+          form.current.reset();
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+          setStatus("Error in sending Email!");
+          setLoading(false);
+        }
+      );
+  };
+
   return (
     <Box className={styles.formMain}>
-        <form ref={form} onSubmit={sendEmail}>
-            <Box className={styles.formBox}>
-                <Text className={styles.label}>Name:</Text>
-                <input className={styles.input} width={"auto"} type="text" name={"user_name"} />
-            </Box>
-            <Box className={styles.formBox}>
-                <Text className={styles.label}>Email:</Text>
-                <input className={styles.input} width={"auto"} type="email" name={"user_email"} />
-            </Box>
-            <Box className={styles.formBox}>
-                <Text className={styles.label}>Message:</Text>
-                <textarea width={"auto"} name={"message"} className={styles.textArea} />
-            </Box>
-            <br />
-            <Input className={styles.btn} width={"auto"} type="submit" value="Send Mail" />
-        </form>
+      <form ref={form} onSubmit={sendEmail}>
+        <Box className={styles.formBox}>
+          <Text className={styles.label}>Name:</Text>
+          <input
+            className={styles.input}
+            type="text"
+            name={"user_name"}
+            required
+          />
+        </Box>
+        <Box className={styles.formBox}>
+          <Text className={styles.label}>Email:</Text>
+          <input
+            className={styles.input}
+            type="email"
+            name={"user_email"}
+            required
+          />
+        </Box>
+        <Box className={styles.formBox}>
+          <Text className={styles.label}>Message:</Text>
+          <textarea
+            name={"message"}
+            className={styles.textArea}
+            required
+          />
+        </Box>
+        <br />
+        <button
+          className={styles.btn}
+          type="submit"
+          isDisabled={loading} 
+          width={"auto"}
+        >
+          {loading ? (
+            <>
+              <Spinner size="sm" marginRight="8px" /> Sending...
+            </>
+          ) : (
+            "Send Mail"
+          )}
+        </button>
+      </form>
+      {status && (
+        <Text mt={1} color={'whitesmoke'}>
+          {status}
+        </Text>
+      )}
     </Box>
-  )
-}
+  );
+};
